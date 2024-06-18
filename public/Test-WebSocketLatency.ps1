23fd41a6c67e3500ef40547527a5334ec32044b4
chenxizhang <ares@xizhang.com>
2024-06-18 21:51:19 +0800

    update to 0.0.2

---
+++ public/Test-WebSocketLatency.ps1
@@ -0,0 +1,11 @@
+function Test-WebSocketLatency {
+    [Parameter(Mandatory = $true)]
+    param([string]$uri)
+    $ws = New-Object System.Net.WebSockets.ClientWebSocket
+    $sw = [System.Diagnostics.Stopwatch]::StartNew()
+    $uri = New-Object System.Uri($uri)
+    $ws.ConnectAsync($uri, [System.Threading.CancellationToken]::None).Wait()
+    $sw.Stop()
+    $ws.CloseAsync().Wait()
+    $sw.ElapsedMilliseconds
+}
